322fc5792301be9bcfbbdd185db5e43b2eb7dfeb
merlinkagu <150964817+merlinkagu@users.noreply.github.com>
2025-10-22 12:44:36 +0300

    Delete git add README.py

--- git add README.py
+++
@@ -1,3 +0,0 @@
-git add README.md
-git commit -m "Mina valisin MIT License-i, kuna MIT on permissive (lubav) litsents: lubab teistel koodi kasutada, muuta ja isegi teha selle põhjal suletud lähtekoodiga versioone — tingimusena tuleb säilitada copyright-märge ja litsentsitekst. See on lühike ja arusaadav ning sobib hästi kursuse õppematerjalide mitmekülgseks jagamiseks ja re-use’iks"
-git push
